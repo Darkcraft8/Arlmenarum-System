73687e136dfaa957b58699495468fa28c15f02fa
Darkcraft8 <phoenixdarkcraft8spqr@gmail.com>
2025-07-05 15:22:49 +0200

    more preparation

---
+++ Arlmenarum/System/oSB/mission/postInit.lua
@@ -0,0 +1,37 @@
+
+local listPath = "/Arlmenarum/System/oSB/mission/list.config"
+local list = assets.json(listPath)
+local count = 0
+local registered = {}
+for kind, missionList in pairs(list) do
+    sb.logInfo("[Arlmenarum System | Fetching Postload]: Registering mission of %s kind", kind)
+    registered[kind] = missionList or {}
+
+    local fileList = assets.byExtension(kind .. "mission")
+    local kindCount = 0
+    for i = 1, #fileList do -- fetch vanilla mission's
+        local jsonFile = fileList[i]
+        local missionName = assets.json(jsonFile)["missionName"]
+        local alreadyExist = false
+        for i = 1, #list do
+            if list[i] == missionName then alreadyExist = true end
+        end
+        if not alreadyExist then
+            table.insert(registered[kind], missionName)
+            count = count + 1
+            kindCount = kindCount + 1
+        end
+    end
+
+    if kindCount > 0 then    
+        sb.logInfo("[Arlmenarum System | Fetching Postload]: Registered %s mission", kindCount)
+    end
+end
+
+if count > 0 then
+    local path = listPath .. ".patch"
+    assets.add(path, registered)
+    assets.patch(listPath, path)
+
+    sb.logInfo("[Arlmenarum System| Fetching Postload] Registered %s mission in total", count)
+end
---
+++ Arlmenarum/System/oSB/objects/recipesHandler.lua
@@ -0,0 +1,71 @@
+local recipeHandler = {}
+function buildRecipesList(interactData) -- we only need to give recipes that are from the vanilla database and/or unlocked through the research system or other
+    local recipes = {}
+    recipeHandler:paneRecipes(recipes, interactData)
+    recipeHandler:researchRecipes(recipes, interactData)
+    recipeHandler:vanillaRecipes(recipes, interactData)
+    return recipes
+end
+
+function recipeHandler:paneRecipes(recipes, interactData)
+    local configJson = interactData.config
+    if type(configJson) == "string" then
+        configJson = root.assetJson(interactData.config)
+    end
+    recipes = interactData.recipes or configJson.recipes or {}
+end
+
+function recipeHandler:researchRecipes(recipes)
+    local testRecipes = {
+        input = {
+            { item = "darkwoodmaterial", count = 6 },
+            { item = "paper", count = 1 },
+            { item = "ironbar", count = 2},
+            { item = "coalore", count = 1}
+        },
+        output = {
+            item = "darkwoodmaterial",
+            count = 1
+        },
+        duration = 1,
+        groups = { "d8Weaponry_gunsmithing_lv1", "d8Weaponry_gunsmithing" }
+    }
+    table.insert(recipes, testRecipes)
+end
+
+function recipeHandler:vanillaRecipes(recipes, interactData)
+    if not root.allRecipes then return nil, sb.logInfo("[recipeHandler] : server doesn't support root.allRecipes, install openStarbound or a fork for full recipeHandler support") end
+    local getAllRecipes = root.allRecipes
+    for _, recipe in ipairs(getAllRecipes("recipe")) do 
+        local add = false
+        for _, filter in ipairs(interactData.filter or {}) do
+            if string.find(sb.printJson(recipe.groups), filter) then add = true break end
+        end
+        if add then table.insert(recipes, recipe) end
+    end
+end
+
+function recipeHandler:allRecipes(filter, recipeList, recipes)
+    local recipeList = recipeList or root.allRecipes()
+    if recipes and recipeList then return end
+    for _, recipe in ipairs(recipeList) do 
+        if type(recipe) == "string" then recipe = root.assetJson(recipe) end
+        local add = false
+        for _, filter in ipairs(interactData.filter or {}) do
+            if string.find(sb.printJson(recipe.groups), filter) then add = true break end
+        end
+        if add then table.insert(recipes, recipe) end
+    end
+end
+--- exemple of usage of result
+
+-- local newRecipesList = buildRecipesList(interactData)
+-- if type(newRecipesList) == "table" then
+--     interactData.recipes = newRecipesList
+-- end
+
+---
+
+-- Todo :
+-- Find why the filters get messed up or manually check recipes for the filters : Done
+-- how vanilla check for filter : if there one filter that is the same add the recipe...
